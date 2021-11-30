60622756ef60b7b4e03f1cd775525517d17d90da
João Pedro de Almeida <78094903+JoaoPedroAC@users.noreply.github.com>
2021-11-30 18:05:37 +0000

    fiz um pequeno teste de aprendizado

---
+++ Atribuir via desestruturacao Obejeto/ex01.js
@@ -0,0 +1,8 @@
+const cestaFrutas = {
+  doce: ['Maçã', 'Pêra', 'Banana', 'Mamão'],
+  azeda: ['limão', 'Laranja', 'Tangerina'],
+  aguada: ['Melancia', 'Melão']
+};
+let {doce} = cestaFrutas;
+let[maca] = doce;
+console.log(maca);
