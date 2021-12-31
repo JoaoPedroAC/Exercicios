56b43ac3848eb30f38f36283c51ea26d442ae821
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-12-30 21:54:55 -0300

    add ex03.js e resolvi as questões

--- Exercicios com funções/ex03.js
+++ Exercicios com funções/ex03.js
@@ -7,3 +7,12 @@ Escreva uma função que receba um numero e retorne o seguinte:
 --- Checar se o número é realmente um número ---
 --- Use a função de 0 a 100 ---
 */
+
+const divisible = (numeric) => {
+	const divisibleThree = numeric % 3 === 0 ? 'Fizz' : 'Não é divisivel por 3';
+	const divisibleFive = numeric % 5 === 0 ? 'Buzz' : 'Não é divisivel por 5';
+	const both = numeric % 3 === 0 && numeric % 5 === 0 ? 'FizzBuzz' : numeric;
+	const checkin = typeof numeric;
+	return `Tipo ${checkin}\n${divisibleThree}\n${divisibleFive}\n${both}`;
+};
+console.log(divisible(Math.round(Math.random() * 100)));
