7067466a729582d1cec3f7704da2dc2398a85545
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-03-03 20:32:58 -0300

    add constructer.js

---
+++ Function/constructor.js
@@ -0,0 +1,16 @@
+// contructor => por convenção precisa-se iniciar com letra maiuscula e precisamos usar obrigatoria mente usar (new)
+//  new cria um objto vazio q eh prenchido com uma variavel
+function Person(nome, sobrenome) {
+	// privados
+	// const Id = 1234;
+	this.nome = nome;
+	this.sobrenome = sobrenome;
+    this.metodo = function(){
+        console.log(`${this.nome}: sou um método`)
+    }
+}
+
+const p1 = new Person('João', 'Pedro');
+const p2 = new Person('Karol', 'Cristina');
+console.log(p1.nome);
+console.log(p2.metodo());
