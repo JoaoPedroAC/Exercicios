7a85dda64cce822df62a78afebe708457e9032f4
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-07-07 21:25:35 -0300

    terminei o Ajax

--- Ajax/assets/js/main.js
+++ Ajax/assets/js/main.js
@@ -1,9 +1,10 @@
 // para fazer o A j a x
+/*
 const request = (obj) => {
 	return new Promise((resolve, reject) => {
 		// XMLHttpRequest = xhr
 		const xhr = new XMLHttpRequest();
-		// aqui vai dar evento de GET, para pegar algo do banco de dados
+		// aqui vai dar evento de GET, para pegar algo do banco de dados, true é para dizer se será ou não async
 		xhr.open(obj.method, obj.url, true);
 		// aqui seria o POST se fosse para mandar algo de um formulario; poder ser assim () ou assim (null), ja que nao enviaremos nada
 		xhr.send();
@@ -17,6 +18,7 @@ const request = (obj) => {
 		});
 	});
 };
+*/
 // aqui ele vai pegar os eventos de click em tudo, porem queremos apenas a ancora
 document.addEventListener('click', (e) => {
 	const element = e.target;
@@ -27,9 +29,16 @@ document.addEventListener('click', (e) => {
 		carregaPagina(element);
 	}
 });
-
+// mande esperar(await) tudo o que tiver que ser Promise
 async function carregaPagina(element) {
 	const href = element.getAttribute('href');
+	const response = await fetch(href);
+	const html = await response.text();
+
+	response.status !== 200 ? Promise.reject('Deu erro') : html;
+	carregeResultado(html);
+	/* 
+	//faz parte do request function
 	const objConfig = {
 		method: 'GET',
 		url: href,
@@ -40,8 +49,30 @@ async function carregaPagina(element) {
 	} catch (error) {
 		console.log(error);
 	}
+	*/
+	// teste com fetch
+	// fetch(href)
+	// 	.then((response) => {
+	// 		return response.status >= 200 && response.status <= 300
+	// 			? response.text()
+	// 			: Promise.reject('Deu erro');
+	// 	})
+	// 	.then((html) => carregeResultado(html))
+	// 	.catch((e) => console.warn(e));
 }
 function carregeResultado(response) {
 	const resultado = document.querySelector('.resultado');
 	resultado.innerHTML = response;
 }
+
+/* Explicação sobre o fetch API (GET) */
+// fetch retorna como sendo Promise
+// fetch('./pagina4.html')
+// 	.then((answer) => {
+// 		if (answer.status !== 200) throw new Error('ERROR 7070 NOSSO');
+// 		return answer.text();
+// 	})
+// 	.then((html) => {
+// 		console.log(html);
+// 	})
+// 	.catch((e) => console.warn(e));
