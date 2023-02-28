6b1db39ae6c37381a27a84725d03021ceea885af
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2023-02-27 22:16:40 -0300

    atualizei o arquivo prototype.js

--- Orientação a Objeto/Prototypes/Prototypes.js
+++ Orientação a Objeto/Prototypes/Prototypes.js
@@ -1,4 +1,4 @@
-/*
+/* // SOBRE O PROTOTYPE // CUIDADO AO MEXER NO PROTOTYPE, POIS O MESMO PODE ACABAR PESANDO SUA APLICAÇÃO
  JS  é baseado em prototipos para passar propriedades e metodos de um obj  para outro;
 
  Definição de prototipo:
@@ -9,6 +9,7 @@
 */
 
 // Construtora -> molde(classe)
+/*
 function Peaple(nome, sobrenome) {
   this.nome = nome;
   this.sobrenome = sobrenome;
@@ -23,3 +24,54 @@ const person1 = new Peaple("João", "Pedro");
 const date = new Date();
 console.log(person1);
 console.log(date);
+*/
+/* // MANIPULANDO O PROTOTYPE
+// new obj -> obj.prototype
+const objA = {
+  keyA: "A",
+  // __PROTO__: Object.prototype é algo que é criado assim que criamos um objeto
+};
+const objB = {
+  keyB: "B",
+  //__PROTO__: objA
+};
+const objC = new Object();
+objC.keyC = "C";
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+console.log(objC);
+*/
+function Produto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
+
+  Produto.prototype.discount = function (percentage = 0) {
+    return (this.preco = this.preco - this.preco * (percentage / 100));
+  };
+  Produto.prototype.addition = function (percentage = 0) {
+    return (this.preco = this.preco + this.preco * (percentage / 100));
+  };
+
+  // Object.freeze(this);
+}
+
+const p1 = new Produto("Camiseta", 50);
+p1.discount(10);
+console.log(p1);
+
+// Literal
+const p2 = {
+  nome: "Casaco",
+  preco: 10, // essa chave tem que ter o mesmo nome da chave de Produto.preco para que possa ser reutilizado o prototype dele!
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.addition(100);
+// console.log(p2);
+
+// Ao meu ver , o lado ruim de usar desta forma é que não está atualizando na hora, somente salvando em uma memoria temporária e é a mesma coisa em caso do objeto estar congelado!
+const p3 = Object.create(Produto.prototype, {
+  nome: { Writable: true, configurable: false, enumerable: true, value: "Blusa Chadrez" },
+  preco: { Writable: true, configurable: false, enumerable: true, value: 80 },
+});
+const valor = p3.discount(50);
+// console.log(valor);
